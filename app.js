13572bacd22a35e59053ea7f6524e7755492c5e1
jvalbuena0317 <146770952+jvalbuena0317@users.noreply.github.com>
2023-11-27 08:54:27 +0000

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const sum = (a,b) => {
+    return a+b
+}
+console.log(sum(7,3))
+
+
+
+
+
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+module.exports = { sum , fromEuroToDollar }
+
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+};
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+};
+
+module.exports = { sum , fromEuroToDollar , fromDollarToYen , fromYenToPound}
+
+
+
